86cad57cb2b46c3a567abaaeb1e0adbcb616856d
Felix Waigner <Waigner.Felix@t-online.de>
2022-02-09 16:53:18 +0100

    Added InstructoinUI. Added OptionsHandMenu. Added Keyboard scripts

--- HoloWorks/Assets/Scripts/Instantiation.cs
+++ HoloWorks/Assets/Scripts/Instantiation.cs
@@ -10,8 +10,11 @@ public class Instantiation : MonoBehaviour
     {
 
         //Instantiate Objects
-        GameObject MaterialObject = Instantiate(MyObject, new Vector3(0, 0, 0), Quaternion.identity);
-        GameObject ConfigObject = Instantiate(Resources.Load<GameObject>("Prefabs/ModelConfigPrefabs/BasicConfigs"));
+        GameObject MaterialObject = Instantiate((MyObject), new Vector3(0, 0, 0), Quaternion.identity);
+        GameObject ConfigObject = Instantiate(Resources.Load<GameObject>("Prefabs/ModelConfigPrefabs/BasicConfigs"), new Vector3(0, 0, 0), Quaternion.identity);
+
+
+       
 
         //Set ConfigurationObject as parent
         MaterialObject.transform.SetParent(ConfigObject.transform);
@@ -19,6 +22,5 @@ public class Instantiation : MonoBehaviour
         //Toggle Active state to apply changes
         ConfigObject.SetActive(false);
         ConfigObject.SetActive(true);
-
     }
 }
