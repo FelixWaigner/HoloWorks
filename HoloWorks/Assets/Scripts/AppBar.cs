6101fe240e7fd109c6da0beb9be2b3220f292149
Felix Waigner <Waigner.Felix@t-online.de>
2022-02-26 19:58:52 +0100

    Added AppBar-Buttons: Transform, Delete, Dublicate

--- HoloWorks/Assets/Scripts/AppBar.cs
+++ HoloWorks/Assets/Scripts/AppBar.cs
@@ -1,3 +1,4 @@
+using Microsoft.MixedReality.Toolkit.UI.BoundsControl;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,5 +21,28 @@ public class AppBar : MonoBehaviour
         //Rotate appbar to match parent
         var rotationVector = RotatingObject.transform.rotation.eulerAngles;
         BasicConfig.transform.rotation = Quaternion.Euler(rotationVector);
+    }
+
+    //Enable and disable the BoundsControl (Rotate and Scale)
+    public void ToggleBoundsControl()
+    {
+        var script = RotatingObject.GetComponent<BoundsControl>();
+        script.enabled = !script.enabled;
+    }
+
+    //Dublicate the Object
+    public void Dublicate()
+    {
+        var obj = Instantiate(BasicConfig);
+
+        //Delete The rigRoot in the Object GameObject (If not deleted the Object is not behaving as it should!)
+        GameObject RigRoot = obj.transform.Find("Object").transform.Find("rigRoot").gameObject;
+        Destroy(RigRoot);
+    }
+
+    //Delete the GameObject
+    public void Delete()
+    {
+        Destroy(BasicConfig);
     }
 }
