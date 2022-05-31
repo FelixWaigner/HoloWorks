551e7471165c8a2ecf9f3c3ac23623eab107c58a
Felix Waigner <Waigner.Felix@t-online.de>
2022-05-31 19:37:28 +0200

    Added save InstructionUI position behaviour

--- HoloWorks/Assets/Scripts/WorkStep.cs
+++ HoloWorks/Assets/Scripts/WorkStep.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class WorkStep : MonoBehaviour
 {
+    public float x, y, z;
+
+    public void SaveUIPosition(GameObject ws)
+    {
+        x = ws.transform.position.x;
+        y = ws.transform.position.y;
+        z = ws.transform.position.z;
+
+        PlayerPrefs.SetFloat("x", x);
+        PlayerPrefs.SetFloat("y", y);
+        PlayerPrefs.SetFloat("z", z);
+    }
+
+    public void LoadUIPosition(GameObject ws)
+    {
+        x = PlayerPrefs.GetFloat("x");
+        y = PlayerPrefs.GetFloat("y");
+        z = PlayerPrefs.GetFloat("z");
+
+        Vector3 LoadPosition = new Vector3(x, y, z);
+        ws.transform.position = LoadPosition;
+    }
     public void CreateNewWorkStep()
     {
         GameObject instruction = Instantiate(Resources.Load<GameObject>("Prefabs/UI/UI"));
@@ -39,9 +61,11 @@ public class WorkStep : MonoBehaviour
         GameObject workstep = gameObject.transform.parent.gameObject;
         GameObject anchor = GameObject.FindWithTag("Anchor");
 
-        if (workstep.transform.GetSiblingIndex() != anchor.transform.childCount)
+        if (workstep.transform.GetSiblingIndex() != anchor.transform.childCount - 1)
         {
+            SaveUIPosition(anchor.transform.GetChild(workstep.transform.GetSiblingIndex()).GetChild(1).gameObject);
             anchor.transform.GetChild(workstep.transform.GetSiblingIndex()).gameObject.SetActive(false);
+            LoadUIPosition(anchor.transform.GetChild(workstep.transform.GetSiblingIndex() + 1).GetChild(1).gameObject);
             anchor.transform.GetChild(workstep.transform.GetSiblingIndex() + 1).gameObject.SetActive(true);
         }
     }
@@ -53,8 +77,9 @@ public class WorkStep : MonoBehaviour
 
         if (workstep.transform.GetSiblingIndex() != 0)
         {
-
+            SaveUIPosition(anchor.transform.GetChild(workstep.transform.GetSiblingIndex()).GetChild(1).gameObject);
             anchor.transform.GetChild(workstep.transform.GetSiblingIndex()).gameObject.SetActive(false);
+            LoadUIPosition(anchor.transform.GetChild(workstep.transform.GetSiblingIndex() - 1).GetChild(1).gameObject);
             anchor.transform.GetChild(workstep.transform.GetSiblingIndex() - 1).gameObject.SetActive(true);
         }
     }
